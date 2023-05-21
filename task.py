676e1f96f30ad0fe085f0dbe25dc547d0136798e
OmarLopGem <101218351+OmarLopGem@users.noreply.github.com>
2023-05-20 18:35:28 -0600

    Detalles finales
    Ya manda el mensaje si el código postal no existe

--- task.py
+++ task.py
@@ -6,29 +6,34 @@ data = pd.read_csv('US_Accidents_Dec21_updated.csv')
 
 # Returns result of task as a dictionary
 def task(zipcode):
-    result = {
-        'zip': zipcode,
-        'most_common': "",
-        'locations': {
+    if data['Zipcode'].isin([zipcode]).any():
+        result = {
+            'zip': zipcode,
+            'most_common': "",
+            'locations': {
+            }
         }
-    }
-    # Filter the data for the specific zipcode
-    filtered_data = data[data['Zipcode'] == zipcode]
+        # Filter the data for the specific zipcode
+        filtered_data = data[data['Zipcode'] == zipcode]
 
-    # Get longitude and latitude of the first five accidents
-    longitudes = filtered_data['Start_Lng'].head(5)
-    latitudes = filtered_data['Start_Lat'].head(5)
+        # Get longitude and latitude of the first five accidents
+        longitudes = filtered_data['Start_Lng'].head(5)
+        latitudes = filtered_data['Start_Lat'].head(5)
 
-    for i, (lat, lon) in enumerate(zip(latitudes, longitudes)):
-        result['locations'][f'l{i}'] = f'https://www.google.com/maps/search/?api=1&query={lat}%2C{lon}'
+        for i, (lat, lon) in enumerate(zip(latitudes, longitudes)):
+            result['locations'][f'l{i}'] = f'https://www.google.com/maps/search/?api=1&query={lat}%2C{lon}'
 
-    # Count the occurrences of each traffic signal
-    traffic_signal_counts = filtered_data['Description'].value_counts()
+        # Count the occurrences of each traffic signal
+        traffic_signal_counts = filtered_data['Description'].value_counts()
 
-    # Get the most common traffic signal
-    result['most_common'] = traffic_signal_counts.idxmax()
+        # Get the most common traffic signal
+        result['most_common'] = traffic_signal_counts.idxmax()
 
-    return result
+        return result
+    else:
+        mssg = ('The zipcode ',zipcode,' does not exist in the dataset')
+        return mssg
+    
 
 # Example result
 # {
